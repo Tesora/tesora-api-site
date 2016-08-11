c7add457e773037d330694b88bab46b2cb21591c
Brian Hunter <bhunter@tesora.com>
2016-08-11 15:42:18 -0400

    add iframe wrapper file
    - add a file that the confluence html include can .. include.
    This file contains the iframe that loads the external content
    - remove absolute urls where i can
    - remove superfulous google analytics script
    
    Change-Id: Ibee1cb2b7dcb7657673b8c08789ed57d61ce9ff0

--- tools/build-tesora-api-ref-database.sh
+++ tools/build-tesora-api-ref-database.sh
@@ -4,6 +4,7 @@
 # and then assembles a new page with this content.
 #
 # This output removes the site navigation and other things we do not need
+# shell,python,javascript,html all in the same file
 
 cat << EOF > /tmp/extract.py
 from bs4 import BeautifulSoup
@@ -13,7 +14,8 @@ print soup.find(id='database-v1').parent
 EOF
 
 SOURCE="publish-docs/api-ref/api-ref-database-v1.html"
-TARGET="./publish-docs/api-ref/tesora-api-ref-database-v1.html"
+TARGET="publish-docs/api-ref/tesora-api-ref-database-v1.html"
+WRAPPER="publish-docs/api-ref/tesora-api-ref-database-v1-iframe.html"
 
 cat << HEADER > /tmp/tesoraapi-header
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
@@ -23,11 +25,11 @@ cat << HEADER > /tmp/tesoraapi-header
       <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"></meta>
       <meta content="width=device-width, initial-scale=1.0" name="viewport"></meta>
       <title>OpenStack API Documentation</title>
-      <link href="http://developer.elasticdb.org/api-ref/apiref/css/bootstrap.min.css" rel="stylesheet"></link>
-      <link href="http://developer.elasticdb.org/api-ref/apiref/css/main.css" rel="stylesheet" type="text/css"></link>
-      <link href="http://developer.elasticdb.org/api-ref/apiref/css/style.css" rel="stylesheet"></link>
-      <link href="http://developer.elasticdb.org/api-ref/apiref/css/main.css" rel="stylesheet" type="text/css"></link>
-      <link href="http://developer.elasticdb.org/api-ref/apiref/css/bootstrap.min.css" rel="stylesheet"></link>
+      <link href="api-ref/apiref/css/bootstrap.min.css" rel="stylesheet"></link>
+      <link href="api-ref/apiref/css/main.css" rel="stylesheet" type="text/css"></link>
+      <link href="api-ref/apiref/css/style.css" rel="stylesheet"></link>
+      <link href="api-ref/apiref/css/main.css" rel="stylesheet" type="text/css"></link>
+      <link href="api-ref/apiref/css/bootstrap.min.css" rel="stylesheet"></link>
    </head>
    <body>
 HEADER
@@ -36,26 +38,20 @@ cat /tmp/tesoraapi-header >> $TARGET
 python /tmp/extract.py $SOURCE >> $TARGET
 
 cat << SCRIPT > /tmp/footerscript
-<script src="apiref/js/jquery-1.10.2.min.js" type="text/javascript"></script><script src="apiref/js/bootstrap.min.js" type="text/javascript"></script><script src="apiref/js/api-site.js" type="text/javascript"></script><script type="text/javascript">
-                    var _gaq = _gaq || [];
-                    _gaq.push(['_setAccount', 'UA-17511903-1']);
-                    _gaq.push(['_setDomainName', '.openstack.org']);
-                    _gaq.push(['_trackPageview']);
-                    (function () {
-                    var ga = document.createElement('script');
-                    ga.type = 'text/javascript';
-                    ga.async = true;
-                    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                    var s = document.getElementsByTagName('script')[0];
-                    s.parentNode.insertBefore(ga, s);
-                    })();
-                  </script>
+<script src="apiref/js/jquery-1.10.2.min.js" type="text/javascript"></script>
+<script src="apiref/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="apiref/js/api-site.js" type="text/javascript"></script>
 </body></html>
 SCRIPT
 
-cat /tmp/footerscript >> ./publish-docs/api-ref/tesora-api-ref-database-v1.html
+cat /tmp/footerscript >> $TARGET
 
 rm /tmp/footerscript
+
+# lastly, generate iframe wrapper
+cat << IFRAME > $WRAPPER
+<iframe frameborder="0" height="650" src="http://developer.elasticdb.org/api-ref/tesora-api-ref-database-v1.html" style="" width="100%"> </iframe></p>
+IFRAME
 
 # clean up all the files we do not care about
 rm publish-docs/api-ref/api-ref*.html
